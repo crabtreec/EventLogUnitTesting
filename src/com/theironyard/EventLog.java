47f58ead3b08ff2e9678fd86325252c989018125
Chad Crabtree <chad.rc.crabtree@gmail.com>
2017-11-02 10:23:12 -0400

    Final

---
+++ src/com/theironyard/EventLog.java
@@ -0,0 +1,41 @@
+package com.theironyard;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.IllegalArgumentException;
+
+
+
+public class EventLog {
+
+
+private static List<Event> eventList = new ArrayList<>();
+
+public static boolean addEvent(Event event) throws IllegalArgumentException {
+
+    if (event.getAction().equals("Face2Face") || event.getAction().equals("PhoneCall") || event.getAction().equals("TextMessaging") || event.getAction().equals("Unknown")) {
+
+    System.out.println("Your event: " + event.getName() + " has been added to the List.");
+    eventList.add(event);
+
+    return true;
+
+    } else {
+
+      throw new IllegalArgumentException();
+
+    }
+ }
+
+public static int getNumEvents() {
+   return eventList.size();
+
+ }
+
+public EventLog() {
+
+ }
+
+}
+
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,6 +3,18 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Event event1 = new Event("Call Sami","PhoneCall");
+         Event event2 = new Event("Chase Dog", "Uh Oh");
+        Event event3 = new Event("Text Cassy", "TextMessaging");
+
+        EventLog.addEvent(event1);
+         //EventLog.addEvent(event2); //This throws the exception.
+        EventLog.addEvent(event3);
+
+        System.out.println("There are " + EventLog.getNumEvents() + " in the list.");
     }
+
 }
+
+
